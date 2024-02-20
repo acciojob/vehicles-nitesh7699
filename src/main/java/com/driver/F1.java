c5ebdb0dd1bd4201452ff8d4d0eaf473101be100
niteshsoni7699 <niteshsoni7699@gmail.com>
2024-02-21 02:42:05 +0530

    initial commit

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,7 +4,8 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-        //super();
+        this.super(name);
+        //super.isManual=isManual;
     }
 
     public void accelerate(int rate){
